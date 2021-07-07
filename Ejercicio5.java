b93c3f79c22c0a888ceca82e64d0ccdc70b7a70f
IzanAbad <82185323+IzanAbad@users.noreply.github.com>
2021-07-07 11:39:40 +0200

    Add files via upload

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -4,6 +4,29 @@ public class Ejercicio5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int A = 10;
+		int B = 15;
+		int C = 44;
+		int D = 5;
+		
+		B = C;
+		System.out.println(B);
+		System.out.println(C);
+
+		C = A;
+		System.out.println(C);
+		System.out.println(A);
+		
+		A = D;
+		System.out.println(A);
+		System.out.println(D);
+		
+		D = B;
+		System.out.println(D);
+		System.out.println(B);
+		
+		
 	}
 
 }
